6d248721fbfee499ff0bd9cbff0f2a32b5ec674d
Hans Hokka <hans.hokka@gmail.com>
2022-02-14 18:31:34 +0200

    puhelinluettelo step5 Filter implemented

--- osa2/puhelinluettelo/src/App.js
+++ osa2/puhelinluettelo/src/App.js
@@ -1,5 +1,14 @@
 import { useState } from "react";
 
+const Filter = (props) => {
+  return (
+    <div>
+      {console.log(props)}
+      filter shown with <input value={props.value} onChange={props.onChange} />
+    </div>
+  );
+};
+const PersonForm = () => {};
 const App = () => {
   const [persons, setPersons] = useState([
     { name: "Arto Hellas", number: "040-123456" },
@@ -65,10 +74,10 @@ const App = () => {
     <div>
       <h2>Phonebook</h2>
       <div>
-        filter shown with{" "}
-        <input value={newFilter} onChange={handleFilterChange} />
+        <Filter value={newFilter} onChange={handleFilterChange} />
       </div>
-      <h2>add a new</h2>
+      <h3>add a new</h3>
+
       <form onSubmit={addName}>
         <div>
           name: <input value={newName} onChange={handleNameChange} />
@@ -80,7 +89,7 @@ const App = () => {
           <button type="submit">add</button>
         </div>
       </form>
-      <h2>Numbers</h2>
+      <h3>Numbers</h3>
       <div>
         {filteredPersons.map((person, i) => (
           <p key={i}>
