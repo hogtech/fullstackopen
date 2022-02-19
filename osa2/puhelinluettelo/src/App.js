cae82ee120e209c670faa608ef62062c94e764e8
Hans Hokka <hans.hokka@gmail.com>
2022-02-19 14:49:57 +0200

    puhelinluettelo step6

--- osa2/puhelinluettelo/src/App.js
+++ osa2/puhelinluettelo/src/App.js
@@ -1,19 +1,31 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import Persons from "./components/Persons";
 import PersonForm from "./components/PersonForm";
 import Filter from "./components/Filter";
+import axios from "axios";
 
 const App = () => {
-  const [persons, setPersons] = useState([
+  /* const [persons, setPersons] = useState([
     { name: "Arto Hellas", number: "040-123456" },
     { name: "Ada Lovelace", number: "39-44-5323523" },
     { name: "Dan Abramov", number: "12-43-234345" },
     { name: "Mary Poppendieck", number: "39-23-6423122" },
-  ]);
+  ]); */
+  const [persons, setPersons] = useState([]);
+
   const [filteredPersons, setFilteredPersons] = useState([{}]);
   const [newName, setNewName] = useState("");
   const [newNumber, setNewNumber] = useState("");
   const [newFilter, setNewFilter] = useState("");
+
+  useEffect(() => {
+    console.log("axios here");
+    axios.get("http://localhost:3001/persons").then((response) => {
+      console.log("promise fulfilled");
+      setPersons(response.data);
+    });
+  }, []);
+  console.log("render", persons.length, "persons");
 
   const addName = (event) => {
     event.preventDefault();
