7d5e3fe197dd17e749d853bc238d1db3937ba060
Hans Hokka <hans.hokka@gmail.com>
2022-03-16 18:20:51 +0200

    working on phonebook and database step 6

--- osa3/index.js
+++ osa3/index.js
@@ -6,6 +6,7 @@ const cors = require("cors");
 require("dotenv").config();
 const Person = require("./models/person");
 const res = require("express/lib/response");
+const { db } = require("./models/person");
 
 app.use(express.static("build"));
 
@@ -40,22 +41,6 @@ app.get("/api/persons", (request, response, next) => {
     .catch((error) => next(error));
 });
 
-/* app.get("/api/persons/:id", (request, response, next) => {
-  Person.findById(request.params.id)
-    .then((person) => {
-      if (person) {
-        response.json(person);
-      } else {
-        response.status(404).end();
-      }
-    })
-    .catch((error) => {
-      console.log(error);
-      response.status(400).send({ error: "malformatted id" });
-    });
-});
- */
-
 app.get("/api/persons/:id", (request, response, next) => {
   Person.findById(request.params.id)
     .then((person) => {
@@ -68,11 +53,6 @@ app.get("/api/persons/:id", (request, response, next) => {
     .catch((error) => next(error));
 });
 
-/* const update = (id, newObject) => {
-  const request = axios.put(`${baseUrl}/${id}`, newObject);
-  return request.then((response) => response.data);
-}; */
-
 app.delete("/api/persons/:id", (request, response, next) => {
   Person.findByIdAndRemove(request.params.id)
     .then((result) => {
@@ -80,12 +60,6 @@ app.delete("/api/persons/:id", (request, response, next) => {
     })
     .catch((error) => next(error));
 });
-
-/* const generateId = () => {
-  const maxId = persons.length > 0 ? Math.max(...persons.map((n) => n.id)) : 0;
-  return maxId + 1;
-};
- */
 
 app.put("/api/persons/:id", (request, response, next) => {
   console.log("body", request.body);
@@ -137,53 +111,13 @@ app.post("/api/persons", (request, response, next) => {
   });
 });
 
-/* app.post("/api/persons", (request, response) => {
-  const body = request.body;
-  console.log("request.body: ", request.body);
-  console.log("persons: ", persons);
-  console.log("persons -> body.name: ", body.name);
-  console.log("Object.values: ", Object.values(persons));
-  console.log("match: " + Object.values(persons[0]).includes("Arto Hellas"));
-  Person.find({}).then((persons) => {
-    res.json(persons);
-  });
+app.get("/info", (request, response, error) => {
+  const today = new Date();
+  const timeUTC = today.toUTCString();
+  const itemsInDb = db.collection.length;
+  console.log(itemsInDb);
 
-  if (persons.some((e) => e.name === request.body.name)) {
-    return response.status(400).json({
-      error: "name must be unique",
-    });
-  }
-
-  if (body.name === "" || !body.name) {
-    return response.status(400).json({
-      error: "name is missing",
-    });
-  }
-  if (body.number === "" || !body.number) {
-    return response.status(400).json({
-      error: "number is missing",
-    });
-  }
-
-  const person = {
-    id: generateId(),
-    name: body.name,
-    number: body.number,
-  };
-
-  person.save().then((savedPerson) => {
-    response.json(savedPerson);
-  });
-
-  persons = persons.concat(person);
-
-  response.json(person);
-}); */
-
-app.get("/info", (req, res) => {
-  today = new Date();
-  var str = today.toUTCString();
-  res.send(`Phonebook has info for ${persons.length} </br> ${str}`);
+  response.send(`Phonebook has info for ${itemsInDb} people </br> ${timeUTC}`);
 });
 
 const unknownEndpoint = (request, response) => {
@@ -202,7 +136,7 @@ const errorHandler = (error, request, response, next) => {
   next(error);
 };
 
-// tämä tulee kaikkien muiden middlewarejen rekisteröinnin jälkeen!
+// After all registered middlewares
 app.use(errorHandler);
 
 const PORT = process.env.PORT || 3001;
