eece705e8ddb82607264bd949a9b756d30295242
Hans Hokka <hans.hokka@gmail.com>
2022-03-28 13:18:31 +0300

    4.4.1

--- osa4/bloglist/utils/list_helper.js
+++ osa4/bloglist/utils/list_helper.js
@@ -1,8 +1,29 @@
+//const blog = require('../models/blog')
+
 const dummy = (blogs) => {
   blogs = 1
   return blogs
 }
 
+/* const totalLikes = blogs => {
+  if (!blogs){
+    return 0
+  }
+  if (blogs.length === 1) {
+    return blogs[0].likes
+  }
+  const reducer = (sum, item) => {
+    return sum + item
+  }
+  return blogs.length === 0
+    ? 0
+    : blogs.reduce(reducer, 0) / blogs.length
+} */
+//let total = 0
+const totalLikes = (blogs) => {
+  return 0
+}
 module.exports = {
-  dummy
+  dummy,
+  totalLikes
 }
