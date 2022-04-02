b6fbaf2b0c347702b509fe1efeeef7a9a5bc169f
Hans Hokka <hans.hokka@gmail.com>
2022-04-02 17:01:56 +0300

    4.6.1

--- osa4/bloglist/utils/list_helper.js
+++ osa4/bloglist/utils/list_helper.js
@@ -1,3 +1,5 @@
+var _ = require('lodash')
+
 const dummy = (blogs) => {
   blogs = 1
   return blogs
@@ -21,11 +23,12 @@ const totalLikes = (blogs) => {
 const favoriteBlog = (blogs) => {
   if (blogs.length === 0){
     return 0
-  }else if (blogs.length === undefined){
+  }/* else if (blogs.length === undefined){
     console.log('undefined')
     return blogs[0]
-  }
+  } */
   const max = blogs.reduce((prev, current) => (prev.likes > current.likes) ? prev : current)
+
   //const index = blogs.findIndex(max)
   const index = blogs.findIndex(object => {
     return object.likes === max.likes
@@ -39,8 +42,76 @@ const favoriteBlog = (blogs) => {
     likes: blogs[index].likes
   }
 }
+
+const mostBlogs = (blogs) => {
+  if (blogs.length === 0){
+    return 0
+  }else if (blogs.length === 1){
+    return {
+      author: blogs[0].author,
+      blogs: 1
+    }
+  }
+  //const max = blogs.reduce((prev, current) => (prev > current) ? prev : current)
+  //const max = _.countBy(blogs)
+  /* const maxObj = blogs.reduce((prev, current) => (prev > current) ? prev : current)
+  const max = _.sumBy(blogs, i => (i.author === 'Edsger W. Dijkstra' ? 1 : 0))
+  console.log('max: ', max)
+  console.log('maxObj.author: ', maxObj.author)
+ *//*
+  const value = 'Edsger W. Dijkstra'
+  const max = blogs.filter((obj) => obj.author === value).length
+  console.log('max: ', max) */
+  /*  const blogsPerAuthor = blogs.reduce((sums,entry) => {
+    sums[entry.author] = (sums[entry.author] || 0) + 1
+    return sums
+  },{})
+  const max = _.maxBy(blogsPerAuthor, 'blogs' )
+  console.log('BlogsPerAuthor: ', blogsPerAuthor) */
+
+  function mode(blogs){
+    return blogs.sort((a,b) =>
+      blogs.filter(v => v===a).length
+      - blogs.filter(v => v===b).length
+    ).pop()
+  }
+
+
+
+  const result = _.values(_.groupBy(blogs)).map(d => ({ name: d[0], count: d.length }))
+  const nameOfTheAuthor = _.maxBy(blogs, 'author').author
+  const numberOfBlogs = _.maxBy(blogs, 'author')
+  console.log('nameOfTheAuthor: ', nameOfTheAuthor)
+  console.log('numberOfBlogs: ', numberOfBlogs)
+  console.log('result: ', result)
+  console.log('mode: ', mode(blogs))
+  return {
+    author: nameOfTheAuthor,
+    blogs: numberOfBlogs
+  }
+}
+
+const mostLikes = (blogs) => {
+  if (blogs.length === 0){
+    return 0
+  }else if (blogs.length === 1){
+    return {
+      author: blogs[0].author,
+      likes: blogs[0].likes
+    }
+  }
+  //const index = 0
+  /* let max = _.maxBy(blogs, 'likes').author
+  console.log('max: ', max) */
+  return {
+    author: _.maxBy(blogs, 'likes').author,
+    likes: _.maxBy(blogs, 'likes').likes
+  }
+}
 module.exports = {
   dummy,
   totalLikes,
-  favoriteBlog
+  favoriteBlog,
+  mostBlogs,
+  mostLikes
 }
