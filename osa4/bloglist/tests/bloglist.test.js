dd9c9c765859964b0bcab8ff2e0b58f2025f9eb7
Hans Hokka <hans.hokka@gmail.com>
2022-04-09 17:49:52 +0300

    4.8

---
+++ osa4/bloglist/tests/bloglist.test.js
@@ -0,0 +1,39 @@
+const mongoose = require('mongoose')
+const supertest = require('supertest')
+const app = require('../app')
+const Blog = require('../models/blog')
+const api = supertest(app)
+const helper = require('./test_helper')
+
+
+/* beforeEach(async () => {
+  await Blog.deleteMany({})
+  let blogObject = new Blog(helper.initialBlogs[0])
+  await blogObject.save()
+  blogObject = new Blog(helper.initialBlogs[1])
+  await blogObject.save()
+  blogObject = new Blog(helper.initialBlogs[2])
+  await blogObject.save()
+})
+ */
+
+beforeEach(async () => {
+  await Blog.deleteMany({})
+  await Blog.insertMany(helper.initialBlogs)
+})
+test('blogs are returned as json', async () => {
+  await api
+    .get('/api/blogs')
+    .expect(200)
+    .expect('Content-Type', /application\/json/)
+})
+
+test('there are three blogs', async () => {
+  const response = await api.get('/api/blogs')
+  //console.log('response body length: ', response.body.length)
+  expect(response.body).toHaveLength(3)
+})
+
+afterAll(() => {
+  mongoose.connection.close()
+})
---
+++ osa4/bloglist/tests/test_helper.js
@@ -0,0 +1,24 @@
+const initialBlogs = [
+  {
+    title: 'First blog',
+    author: 'Hans Hokka',
+    url: 'www.google.fi',
+    likes: 1
+  },
+  {
+    title: 'Second blog',
+    author: 'Hans Hokka',
+    url: 'www.fi',
+    likes: 10
+  },
+  {
+    title: 'Third blog',
+    author: 'Hans Hokka',
+    url: 'www.aalto.fi',
+    likes: 15
+  }
+
+]
+module.exports = {
+  initialBlogs
+}
