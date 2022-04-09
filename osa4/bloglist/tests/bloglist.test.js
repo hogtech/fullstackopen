5a6e0e85a441c5c33347b514cf3942f773e7e810
Hans Hokka <hans.hokka@gmail.com>
2022-04-09 19:51:54 +0300

    4.10

--- osa4/bloglist/tests/bloglist.test.js
+++ osa4/bloglist/tests/bloglist.test.js
@@ -31,13 +31,36 @@ test('blogs are returned as json', async () => {
 test('there are three blogs', async () => {
   const response = await api.get('/api/blogs')
   //console.log('response body length: ', response.body.length)
-  expect(response.body).toHaveLength(3)
+  expect(response.body).toHaveLength(helper.initialBlogs.length)
 })
 
 test('the name of the id is id instead of _id', async () => {
   const response = await api.get('/api/blogs')
   //console.log('response body [0].id', response.body[0].id)
   expect(response.body[0].id).toBeDefined()
+})
+
+test('a valid blog can be added ', async () => {
+  const newBlog = {
+    title: 'First blog',
+    author: 'Hans Hokka',
+    url: 'www.google.fi',
+    likes: 1
+  }
+
+  await api
+    .post('/api/blogs')
+    .send(newBlog)
+    .expect(201)
+    .expect('Content-Type', /application\/json/)
+
+  const blogsAtEnd = await helper.blogsInDb()
+  expect(blogsAtEnd).toHaveLength(helper.initialBlogs.length + 1)
+
+  const title = blogsAtEnd.map(n => n.title)
+  expect(title).toContain(
+    'First blog'
+  )
 })
 afterAll(() => {
   mongoose.connection.close()
