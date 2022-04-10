0e6510e7ef7abf6a13a8cfde43f2879348498560
Hans Hokka <hans.hokka@gmail.com>
2022-04-10 13:21:57 +0300

    4.12

--- osa4/bloglist/controllers/blogs.js
+++ osa4/bloglist/controllers/blogs.js
@@ -1,25 +1,23 @@
 const blogsRouter = require('express').Router()
 const Blog = require('../models/blog')
-blogsRouter.get('/', (request, response) => {
-  Blog
-    .find({})
-    .then(blogs => {
-      response.json(blogs)
-    })
+
+blogsRouter.get('/', async (request, response) => {
+  const blogs = await Blog.find({})
+  response.json(blogs)
 })
 
-blogsRouter.post('/', (request, response) => {
+blogsRouter.post('/', async (request, response) => {
   const blog = new Blog(request.body)
-  //console.log('response[0].body.title: ', response[0].body.title)
   if (blog.likes === undefined){
     blog.likes = 0
   }
-  //console.log('blog.likes: ', blog.likes)
-  blog
-    .save()
-    .then(result => {
-      response.status(201).json(result)
-    })
+  if (blog.title === undefined || blog.url === undefined){
+    //const savedBlog = await blog.save()
+    response.status(400).json(request.body)
+  } else {
+    const savedBlog = await blog.save()
+    response.status(201).json(savedBlog)
+  }
 })
 
 module.exports = blogsRouter
--- osa4/bloglist/tests/bloglist.test.js
+++ osa4/bloglist/tests/bloglist.test.js
@@ -85,6 +85,23 @@ test('blog without likes is given 0 for likes', async () => {
 
 })
 
+test('blog without title and url is not added', async () => {
+  const newBlog = {
+    author: 'Hans Hokka',
+    likes: 40
+  }
+
+  await api
+    .post('/api/blogs')
+    .send(newBlog)
+    .expect(400)
+
+  const blogsAtEnd = await helper.blogsInDb()
+
+  expect(blogsAtEnd).toHaveLength(helper.initialBlogs.length)
+})
+
+
 afterAll(() => {
   mongoose.connection.close()
 })
