2e5ab69a8edfafc18d0e63aa2b4f65163515270c
Hans Hokka <hans.hokka@gmail.com>
2022-04-19 18:03:08 +0300

    4.15.1

--- osa4/bloglist/tests/test_helper.js
+++ osa4/bloglist/tests/test_helper.js
@@ -1,4 +1,5 @@
 const Blog = require('../models/blog')
+const User = require('../models/user')
 
 const initialBlogs = [
   {
@@ -22,13 +23,28 @@ const initialBlogs = [
 
 ]
 
+const nonExistingId = async () => {
+  const note = new User({ content: 'willremovethissoon', date: new Date() })
+  await note.save()
+  await note.remove()
+
+  return note._id.toString()
+}
+
 const blogsInDb = async () => {
   const blogs = await Blog.find({})
   return blogs.map(blog => blog.toJSON())
 }
 
+const usersInDb = async () => {
+  const users = await User.find({})
+  return users.map(u => u.toJSON())
+}
+
 
 module.exports = {
   initialBlogs,
-  blogsInDb
+  nonExistingId,
+  blogsInDb,
+  usersInDb
 }
