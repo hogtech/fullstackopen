2e8c6cfd25f8bf64eb50cc652d642efec006bb68
Hans Hokka <hans.hokka@gmail.com>
2022-05-19 15:50:07 +0300

    5.9

--- osa5/bloglist-frontend/src/App.js
+++ osa5/bloglist-frontend/src/App.js
@@ -84,13 +84,15 @@ const App = () => {
     <div>
       <h2>blogs</h2>
 
-      {blogs.map(blog =>
-        <Blog
-          key={blog.id}
-          blog={blog}
-          makeLikeCallback={like}
-        />
-      )}
+      {blogs
+        .sort ((a, b) => a.likes > b.likes ? 1 : -1)
+        .map(blog =>
+          <Blog
+            key={blog.id}
+            blog={blog}
+            makeLikeCallback={like}
+          />
+        )}
 
     </div>
   )
