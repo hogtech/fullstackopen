2f9312ac57df33345b31fe9e7f46502b2fd0eab3
Hans Hokka <hans.hokka@gmail.com>
2022-06-04 17:47:46 +0300

    6.3

--- osa6/redux-anecdotes-main/src/App.js
+++ osa6/redux-anecdotes-main/src/App.js
@@ -1,15 +1,14 @@
 import { useSelector, useDispatch } from 'react-redux'
 import { createStore } from 'redux'
-
+import { useEffect } from 'react'
 import reducer from './reducers/anecdoteReducer'
 
-const store = createStore(reducer)
 const App = () => {
   const anecdotes = useSelector(state => state)
   const dispatch = useDispatch()
 
   const vote = (id) => {
-    store.dispatch({
+    dispatch({
       type: 'VOTE',
       id: id
     })
