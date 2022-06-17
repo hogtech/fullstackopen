3efcbf3fda1c0deb36fdfcbc6d23831639dd70ed
Hans Hokka <hans.hokka@gmail.com>
2022-06-17 11:15:30 +0300

    6.19

--- osa6/redux-anecdotes-main/src/components/Notification.js
+++ osa6/redux-anecdotes-main/src/components/Notification.js
@@ -1,7 +1,9 @@
-import { useSelector } from 'react-redux'
+//import { useSelector } from 'react-redux'
+import { connect } from 'react-redux'
 
-const Notification = () => {
-  const notification = useSelector(state => state.notification)
+const Notification = (props) => {
+  //const notification = useSelector(state => state.notification)
+  const notification = props.notification
   let style = {}
   if (notification !== '') {
     style = {
@@ -21,5 +23,11 @@ const Notification = () => {
     </div>
   )
 }
+const mapStateToProps = (state) => {
+  return {
+    notification: state.notification
+  }
+}
 
-export default Notification
+const ConnectedNotification = connect(mapStateToProps)(Notification)
+export default ConnectedNotification
